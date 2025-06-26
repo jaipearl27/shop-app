cffc7feab7ce5332f4837b675fa042d295f23403
jaipearl27 <jaix2793@gmail.com>
2025-06-26 16:24:08 +0530

    apiUrl update

--- app/shopify.server.js
+++ app/shopify.server.js
@@ -15,7 +15,7 @@ const shopify = shopifyApp({
   apiSecretKey: process.env.SHOPIFY_API_SECRET || "",
   apiVersion: ApiVersion.January25,
   scopes: process.env.SCOPES?.split(","),
-  appUrl: process.env.SHOPIFY_APP_URL || "",
+  appUrl: process.env.SHOPIFY_APP_URL || "https://shopify.pearl-developer.com",
   authPathPrefix: "/auth",
   sessionStorage: new PrismaSessionStorage(prisma),
   distribution: AppDistribution.AppStore,
